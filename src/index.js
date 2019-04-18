208553322599d869de3d072835f132f103bac57e
ChilenoSika <jaum97@yahoo.com.br>
2019-04-18 00:05:36 -0300

    ReduceBy

--- src/index.js
+++ src/index.js
@@ -15,16 +15,46 @@ const add8 = R.compose(
 const test2 = add8(8);
 
 const arr = [
-  { id: 1, name: "fluffkins" },
-  { id: 2, name: "bob" },
-  { id: 3, name: "jerry" },
-  { id: 4, name: "ermin" },
-  { id: 5, name: "joseph" },
-  { id: 6, name: "mike" }
+  { id: 1, other: { name: "fluffkins" } },
+  { id: 2, other: { name: "bob" } },
+  { id: 3, other: { name: "jerry" } },
+  { id: 4, other: { name: "ermin" } },
+  { id: 5, other: { name: "joseph" } },
+  { id: 6, other: { name: "mike" } }
 ];
 
-const potato = R.pluck("name");
+const potato = R.pluck("name", R.pluck("other", arr));
 
-const test = potato(arr);
+const abby = { name: "Abby", age: 7, hair: "blond" };
+const fred = { name: "Fred", age: 12, hair: "brown" };
+const rusty = { name: "Rusty", age: 10, hair: "brown" };
+const alois = { name: "Alois", age: 15, disposition: "surly" };
+const kids = [abby, fred, rusty, alois];
+const hasBrownHair = R.propEq("hair", "brown");
+const potato = R.filter(hasBrownHair, kids);
 
-test;
+potato;
+
+const groupNames = (acc, { name }) => acc.concat(name);
+const groupScore = (acc, { score }) => acc.concat(score);
+const toGrade = ({ score }) =>
+  score < 65
+    ? "F"
+    : score < 70
+    ? "D"
+    : score < 80
+    ? "C"
+    : score < 90
+    ? "B"
+    : "A";
+
+var students = [
+  { name: "Abby", score: 83 },
+  { name: "Bart", score: 62 },
+  { name: "Curt", score: 88 },
+  { name: "Dora", score: 92 }
+];
+
+const maninho = R.reduceBy(groupScore, [], toGrade, students);
+
+maninho;
