8294d75aaa40916415bf68cf11c9b0a130766475
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-05-23 23:38:15 -0300

    GroupBy Commit

---
+++ src/groupBy.js
@@ -0,0 +1,18 @@
+const byGrade = R.groupBy(function(student) {
+  const score = student.score;
+  return score < 65 ? 'F' :
+         score < 70 ? 'D' :
+         score < 80 ? 'C' :
+         score < 90 ? 'B' : 'A';
+});
+const students = [{name: 'Abby', score: 84},
+                {name: 'Eddy', score: 58},
+                // ...
+                {name: 'Jack', score: 69}];
+byGrade(students);
+// {
+//   'A': [{name: 'Dianne', score: 99}],
+//   'B': [{name: 'Abby', score: 84}]
+//   // ...,
+//   'F': [{name: 'Eddy', score: 58}]
+// }
